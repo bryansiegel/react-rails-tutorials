8b064e0126994642fa691d88a5d64d6a33fb430b
Bryan Siegel <bryansiegel@gmail.com>
2020-04-17 13:14:44 -0700

    data from rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+course = Course.create(title: 'Hello World', description: 'Create A react app with Ruby on Rails')
+
+section = Section.create(title: 'Chapter 1', course: course)
+
+episodes = Episode.create([
+                    {
+                    title: '1. Setting Up A New Ruby On Rails App With React',
+                    description: 'lorem ipsum',
+                    url: 'https://www.youtube.com/embed/B0SxxHAImhc',
+                    section: section
+                }, {
+                    title: '2. Adding React to an Existing Rails App',
+                    description: 'lorem ipsum',
+                    url: 'https://www.youtube.com/embed/B0SxxHAImhc',
+                    section: section
+                },
+                {
+                    title: '3. Building A Hello World App',
+                    description: 'lorem ipsum',
+                    url: 'https://www.youtube.com/embed/B0SxxHAImhc',
+                    section: section
+                },
+                {
+                    title: '4. Adding React Router Dom to your app',
+                    description: 'lorem ipsum',
+                    url: 'https://www.youtube.com/embed/B0SxxHAImhc',
+                    section: section
+                },
+])
