77f0e68a17b6de96c9a3c61253723901d09b48b1
Bryan Siegel <bryansiegel@gmail.com>
2020-04-17 11:58:09 -0700

    Item component

--- app/javascript/components/home/Table/Thumbnail.js
+++ app/javascript/components/home/Table/Thumbnail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 const Thumbnail = () => {
     return(
-        <div>
+        <div className="pt-4 pb-4">
             <iframe width = "100%"
             height = "100%"
             src = "https://www.youtube.com/embed/B0SxxHAImhc"
